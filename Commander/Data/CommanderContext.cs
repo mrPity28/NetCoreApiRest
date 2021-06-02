ef4ac8a7070b790be5616ddd4c6dfe8b6d6093ec
mrPity28 <marvalp.dev@gmail.com>
2021-06-02 11:36:43 -0400

    fin migraciones
    Agregar remover y aplicar migración

--- Commander/Data/CommanderContext.cs
+++ Commander/Data/CommanderContext.cs
@@ -5,10 +5,11 @@ namespace Commander.Data
 {
     public class CommanderContext : DbContext
     {
-        public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt)
+        public CommanderContext(DbContextOptions<CommanderContext>opt) : base(opt)
         {
-            
+
         }
+
         
         public DbSet<Command> Commands { get; set; }
     }
